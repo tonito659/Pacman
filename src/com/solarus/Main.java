5727f99344c6710784b12d2080ea557ed75123a7
Nico <nicolas.benmennad@isep.fr>
2017-01-09 15:50:43 +0100

    map fini

--- src/com/solarus/Main.java
+++ src/com/solarus/Main.java
@@ -6,8 +6,8 @@ import java.awt.*;
 public class Main {
 
 
-    public final static int X_MAX= 30;
-    public final static int Y_MAX= 30;
+    public final static int X_MAX= 32;
+    public final static int Y_MAX= 32;
     public final static float WIDTH= 1;
 
 
@@ -17,32 +17,20 @@ public class Main {
 
 
 
-        StdDraw.setCanvasSize(500,500);
+        StdDraw.setCanvasSize(600,600);
         StdDraw.setXscale(-WIDTH, X_MAX + WIDTH);
         StdDraw.setYscale(-WIDTH, Y_MAX + WIDTH);
-        int [][] map = Map.getLabyrinthe();
-        int taillex_map = map.length;
-        int tailley_map = map[1].length;
+        //int [][] map = Map.getLabyrinthe();
+        //int taillex_map = map.length;
+        //int tailley_map = map[1].length;
+        StdDraw.filledRectangle(14,14,15,17);//fond noir
+        Map.generateurGraphique(Map.getLabyrinthe());
 
-        System.out.println("X = " + taillex_map);
-        System.out.println("Y = " + tailley_map);
+        //System.out.println("X = " + taillex_map);
+        //System.out.println("Y = " + tailley_map);
 
-        StdDraw.filledRectangle(14,14,15,15);
-        for (int dang_y=26; dang_y>=0; --dang_y){
-            for(int dang_x=27; dang_x>=0; --dang_x)  {
-                if (map[dang_y][dang_x] == 0) {
-                    StdDraw.setPenColor(Color.blue);
-                    StdDraw.filledRectangle(dang_x, dang_y, 0.51, 0.51);
-                }else if(map[dang_y][dang_x] == 2) {
-                    StdDraw.setPenColor(Color.orange);
-                    StdDraw.filledCircle(dang_x,dang_y,0.1);
-                }else if(map[dang_y][dang_x] == 3) {
-                    StdDraw.setPenColor(Color.yellow);
-                    StdDraw.filledCircle(dang_x, dang_y, 0.3);
-                }
-                System.out.println("X = " + dang_x + " Y: " + dang_y);
-            }
-        }
+
+
         StdDraw.filledCircle(0,0,3);
 
     }
