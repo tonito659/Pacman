3e0d58dad9d71e46a9e12d1548c2689d96ee651d
Antoine <fallenhaswon@gmail.com>
2017-01-10 15:27:50 +0100

    Debut mouvements fantomes

--- src/com/solarus/Fantome.java
+++ src/com/solarus/Fantome.java
@@ -1,18 +1,46 @@
 package com.solarus;
 
+import java.awt.*;
+
 public class Fantome extends Entity{
 
     private boolean isInvicible = true;
-    private String couleur = "";
+    private Color couleur;
+    private int px, py;
+    private int offset;
 
-    public static void deplacementRouge(){
-
+    public Fantome(Color couleur, int abcisse, int ordonnee) {
+        //init fantome, on peut les placer sur des endroits random dans la map
+        this.couleur = couleur;
+        this.px = abcisse;
+        this.py = ordonnee;
     }
 
-    public static void deplacementBleu(){
 
+    public void deplacement(){
+        //
+        double seed = Math.random();
+
+        if (true){ //placeholder
+
+        }
     }
 
+    public boolean isIntersection(){
+        // On teste si l'emplacement actuel du fantome est une intersection
+        boolean intersect= false;
+        int [][] intersectMap = Map.getLabyrinthe()
+
+        if (intersectMap[this.px+1][this.py] =! 0
+                || intersectMap[this.px][this.py] =! 0
+                || intersectMap[this.px][this.py] =! 0
+                || intersectMap[this.px][this.py] =! 0 )
+            {
+            intersect = true;
+
+        }
+        return intersect;
+    }
     public static void deplacementOrange(){
 
     }
