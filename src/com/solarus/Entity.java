50b1dd30104bc1cc7a801a5bbd6ddc566831e8dc
Antoine <fallenhaswon@gmail.com>
2017-01-11 15:26:27 +0100

    Ajout des méthodes dans entity et dans joueur

--- src/com/solarus/Entity.java
+++ src/com/solarus/Entity.java
@@ -3,24 +3,51 @@ package com.solarus;
 
 public class Entity {
 
-    private int x, y;
+    private int px, py;
 
     public int getX() {
-        return x;
+        return px;
     }
 
     public void setX(int x) {
-        this.x = x;
+        this.px = x;
     }
 
     public int getY() {
-        return y;
+        return py;
     }
 
     public void setY(int y) {
-        this.y = y;
+        this.py = y;
     }
 
 
     //public void deplacement(){}
+    public void transfertBord(){
+        // DO YOU EVEN TP BRO ?
+        // INSTANT TP OMGHAXX
+        if(this.px == 0 && this.py == 16){
+            px=26;
+        } else if (this.px == 26 && this.py == 16){
+            px=0;
+        }
+    }
+
+    public boolean [] collisionsPossibles(){
+        //indique les collisions, dans l'ordre : N, E, S, O
+        boolean [] collision = {false,false,false,false} ;
+        int [][] temp = Map.getLabyrinthe();
+        if(temp[this.py-1][this.px]==0){ //nord
+            collision[0] = true;
+        } else if(temp[this.py][this.px+1]==0){ //est
+            collision[1] = true;
+        } else if(temp[this.py+1][this.px]==0){ //sud
+            collision[2] = true;
+        } else if(temp[this.py][this.px+1]==0){ //ouest
+            collision[3] = true;
+        }
+
+        return collision;
+    }
+
 }
--- src/com/solarus/Joueur.java
+++ src/com/solarus/Joueur.java
@@ -5,10 +5,18 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Joueur extends Entity{
 
+    // Concerne les données sur le joueur
 
     private boolean hasMegaGraine =false;
     private int nbVie = 1;
     private int score = 0;
     private String pseudo = "";
+
+
+    // Concerne les attributs du pacman
+
+    private int px, py;
+
+
 
 }
